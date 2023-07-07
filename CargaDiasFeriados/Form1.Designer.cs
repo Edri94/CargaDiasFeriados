73c9d440613035809c53b86d1b7ad57cd58e53c4
edrialan.angulo <edrialan.angulo@bbva.com>
2023-07-07 13:25:08 -0600

    first commit

---
+++ CargaDiasFeriados/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace CargaDiasFeriados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbAmbos = new System.Windows.Forms.RadioButton();
+            this.rbEUA = new System.Windows.Forms.RadioButton();
+            this.rbMexico = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbAño = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMes = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpMesFestivo = new System.Windows.Forms.MonthCalendar();
+            this.brnCancelar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dtGrdVwFeriados = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrdVwFeriados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbAmbos);
+            this.groupBox1.Controls.Add(this.rbEUA);
+            this.groupBox1.Controls.Add(this.rbMexico);
+            this.groupBox1.Location = new System.Drawing.Point(16, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(993, 82);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione el pais";
+            // 
+            // rbAmbos
+            // 
+            this.rbAmbos.AutoSize = true;
+            this.rbAmbos.Location = new System.Drawing.Point(596, 37);
+            this.rbAmbos.Name = "rbAmbos";
+            this.rbAmbos.Size = new System.Drawing.Size(84, 24);
+            this.rbAmbos.TabIndex = 0;
+            this.rbAmbos.TabStop = true;
+            this.rbAmbos.Text = "Ambos";
+            this.rbAmbos.UseVisualStyleBackColor = true;
+            // 
+            // rbEUA
+            // 
+            this.rbEUA.AutoSize = true;
+            this.rbEUA.Location = new System.Drawing.Point(319, 37);
+            this.rbEUA.Name = "rbEUA";
+            this.rbEUA.Size = new System.Drawing.Size(147, 24);
+            this.rbEUA.TabIndex = 0;
+            this.rbEUA.TabStop = true;
+            this.rbEUA.Text = "Estados Unidos";
+            this.rbEUA.UseVisualStyleBackColor = true;
+            // 
+            // rbMexico
+            // 
+            this.rbMexico.AutoSize = true;
+            this.rbMexico.Location = new System.Drawing.Point(42, 37);
+            this.rbMexico.Name = "rbMexico";
+            this.rbMexico.Size = new System.Drawing.Size(83, 24);
+            this.rbMexico.TabIndex = 0;
+            this.rbMexico.TabStop = true;
+            this.rbMexico.Text = "Mexico";
+            this.rbMexico.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbAño);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.cmbMes);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(16, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(993, 85);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Selecciones el Mes y Año a visualizar";
+            // 
+            // cmbAño
+            // 
+            this.cmbAño.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAño.FormattingEnabled = true;
+            this.cmbAño.Location = new System.Drawing.Point(527, 36);
+            this.cmbAño.Name = "cmbAño";
+            this.cmbAño.Size = new System.Drawing.Size(224, 28);
+            this.cmbAño.TabIndex = 1;
+            this.cmbAño.SelectedIndexChanged += new System.EventHandler(this.cmbAño_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(459, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Año:";
+            // 
+            // cmbMes
+            // 
+            this.cmbMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMes.FormattingEnabled = true;
+            this.cmbMes.Location = new System.Drawing.Point(92, 36);
+            this.cmbMes.Name = "cmbMes";
+            this.cmbMes.Size = new System.Drawing.Size(224, 28);
+            this.cmbMes.TabIndex = 1;
+            this.cmbMes.SelectedIndexChanged += new System.EventHandler(this.cmbMes_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mes:";
+            // 
+            // dtpMesFestivo
+            // 
+            this.dtpMesFestivo.Location = new System.Drawing.Point(16, 228);
+            this.dtpMesFestivo.Name = "dtpMesFestivo";
+            this.dtpMesFestivo.TabIndex = 2;
+            // 
+            // brnCancelar
+            // 
+            this.brnCancelar.Location = new System.Drawing.Point(714, 510);
+            this.brnCancelar.Name = "brnCancelar";
+            this.brnCancelar.Size = new System.Drawing.Size(140, 44);
+            this.brnCancelar.TabIndex = 3;
+            this.brnCancelar.Text = "Cancelar";
+            this.brnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(869, 510);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(140, 44);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(559, 510);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(140, 44);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dtGrdVwFeriados
+            // 
+            this.dtGrdVwFeriados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGrdVwFeriados.Location = new System.Drawing.Point(402, 228);
+            this.dtGrdVwFeriados.Name = "dtGrdVwFeriados";
+            this.dtGrdVwFeriados.RowHeadersWidth = 62;
+            this.dtGrdVwFeriados.RowTemplate.Height = 28;
+            this.dtGrdVwFeriados.Size = new System.Drawing.Size(607, 253);
+            this.dtGrdVwFeriados.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 566);
+            this.Controls.Add(this.dtGrdVwFeriados);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.brnCancelar);
+            this.Controls.Add(this.dtpMesFestivo);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrdVwFeriados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbAmbos;
+        private System.Windows.Forms.RadioButton rbEUA;
+        private System.Windows.Forms.RadioButton rbMexico;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbAño;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar dtpMesFestivo;
+        private System.Windows.Forms.Button brnCancelar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dtGrdVwFeriados;
+    }
+}
+
---
+++ CargaDiasFeriados/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargaDiasFeriados
+{
+    public partial class Form1 : Form
+    {
+
+        List<Fecha> meses;
+        List<Fecha> años;
+        List<DIAS_FERIADOS> dias_feriados;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                meses = new List<Fecha>();
+                años = new List<Fecha>();
+                dias_feriados = new List<DIAS_FERIADOS>();
+
+                LlenarMeses();
+                LlenarAños();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error en Load", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }          
+        }
+
+        private void LlenarAños()
+        {
+            DateTime primer_fecha = new DateTime(DateTime.Now.Year, 1, 1);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                años.Add(new Fecha { Nombre = primer_fecha.ToString("yyyy"), Numero = primer_fecha.Year });
+                primer_fecha = primer_fecha.AddYears(1);
+            }
+
+            cmbAño.DataSource = años;
+            cmbAño.ValueMember = "Numero";
+            cmbAño.DisplayMember = "Nombre";
+        }
+
+        private void LlenarMeses()
+        {
+            DateTime primer_fecha = new DateTime(DateTime.Now.Year, 1, 1);
+
+            for (int i = 1; i <= 12; i++)
+            {
+                meses.Add(new Fecha { Nombre = primer_fecha.ToString("MMMM"), Numero = primer_fecha.Month });
+                primer_fecha = primer_fecha.AddMonths(1);
+            }
+
+            cmbMes.DataSource = meses;
+            cmbMes.ValueMember = "Numero";
+            cmbMes.DisplayMember = "Nombre";
+        }
+
+        private class Fecha
+        {
+            public int Numero { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        private void cmbMes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SeleccionarCalendario();
+        }
+
+        private void cmbAño_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SeleccionarCalendario();
+        }
+
+        private void SeleccionarCalendario()
+        {
+            try
+            {
+                if (cmbAño.SelectedValue != null && cmbMes.SelectedValue != null)
+                {
+                    int año = ((Fecha)cmbAño.SelectedItem).Numero;
+                    int mes = ((Fecha)cmbMes.SelectedItem).Numero;
+
+                    dtpMesFestivo.SelectionStart = new DateTime(año, mes, 1);
+
+                    dias_feriados = ConsultaFeriadosPorMes(new DateTime(año, mes, 1));
+
+                    DataTable dt = LlenarListFeriados(dias_feriados);
+                    dtGrdVwFeriados.DataSource = dt;
+                    dtGrdVwFeriados.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    dtGrdVwFeriados.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    dtGrdVwFeriados.Refresh();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error en Calendario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private DataTable LlenarListFeriados(List<DIAS_FERIADOS> dias_feriados)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Fecha");
+            dt.Columns.Add("Tipo");
+
+            foreach (DIAS_FERIADOS dia in dias_feriados)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Fecha"] = dia.fecha.ToString("dd-MM-yyyy");
+
+                if(dia.tipo_dia_feriado == 1)
+                {
+                    dr["Tipo"] = "Mexico";
+                }
+                else if (dia.tipo_dia_feriado == 2)
+                {
+                    dr["Tipo"] = "Estados Unidos";
+                }
+                else if (dia.tipo_dia_feriado == 3)
+                {
+                    dr["Tipo"] = "Ambos";
+                }
+
+                dt.Rows.Add(dr);
+
+            }
+
+            return dt;
+        }
+
+        private List<DIAS_FERIADOS> ConsultaFeriadosPorMes(DateTime dateTime)
+        {
+            using (CATALOGOSEntities context = new CATALOGOSEntities())
+            {
+                DateTime fecha1 = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day);
+                DateTime fecha2 = new DateTime(dateTime.Year, dateTime.Month, UltimoDiaMes(dateTime));
+
+                List < DIAS_FERIADOS > resultado =
+                    context.DIAS_FERIADOS
+                    .Where(w =>
+                        w.fecha >= fecha1
+                        &&
+                        w.fecha <= fecha2
+                    )
+                    .OrderBy(o => o.fecha)
+                    .ToList();
+
+                return resultado;
+                
+            }
+        }
+
+        private int UltimoDiaMes(DateTime dateTime)
+        {
+            int dia = 0;
+            DateTime fecha_sum = dateTime;
+
+            do
+            {
+                fecha_sum = fecha_sum.AddDays(1);
+                dia++;
+
+            } while (fecha_sum.Month == dateTime.Month);
+
+            return dia;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DIAS_FERIADOS dia = new DIAS_FERIADOS();
+
+                if(rbMexico.Checked)
+                {
+                    dia.tipo_dia_feriado = 1;
+                }
+                else if(rbEUA.Checked)
+                {
+                    dia.tipo_dia_feriado = 2;
+                }
+                else if(rbAmbos.Checked)
+                {
+                    dia.tipo_dia_feriado = 3;
+                }
+
+                dia.fecha = dtpMesFestivo.SelectionStart;
+
+
+                using (CATALOGOSEntities context = new CATALOGOSEntities())
+                {
+                    SqlConnection cnn = (SqlConnection) context.Database.Connection;
+
+                    cnn.Open();
+                    SqlCommand cmd = cnn.CreateCommand();
+                    cmd.CommandText = "INSERT INTO DIAS_FERIADOS(fecha, tipo_dia_feriado) VALUES(@param1, @param2)";
+                    cmd.Parameters.AddWithValue("@param1", dia.fecha);
+                    cmd.Parameters.AddWithValue("@param2", dia.tipo_dia_feriado);
+                    int afectados = cmd.ExecuteNonQuery();
+
+                    if(afectados > 0)
+                    {
+                        SeleccionarCalendario();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error al Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        
+    } 
+}
